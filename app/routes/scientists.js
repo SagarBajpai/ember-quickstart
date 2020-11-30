ea1a616e6bdd35bdc3fd4767b9ba4fee57b01356
sagarbajpai <ashversache@gmail.com>
2020-11-30 11:34:25 +0530

    Feat(scientists): Rendering list using #each

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,4 +1,7 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default class ScientistsRoute extends Route {
+  model() {
+    return ["Ankush", "Sagar", "Nikhil", "Ankur"];
+  }
 }
